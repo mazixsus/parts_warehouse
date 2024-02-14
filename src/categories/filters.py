eb2a4e3de649c37d9e7d3388c19f79ab11b08d04
mateusz <mateusz3249@gmail.com>
2024-02-14 21:40:33 +0100

    categories search small correction

---
+++ src/categories/filters.py
@@ -0,0 +1,18 @@
+from rest_framework import filters
+
+class CategoriesFilterBackend(filters.BaseFilterBackend):
+    """
+    Filter that only allows users to see their own objects.
+    """
+    def filter_queryset(self, request, queryset, view):
+        print(request)
+        name = request.query_params.get('name')
+        parent = request.query_params.get('parent')
+
+        if name:
+            queryset = queryset.filter(name=name)
+
+        if parent:
+            queryset = queryset.filter(parent=parent)
+
+        return queryset
