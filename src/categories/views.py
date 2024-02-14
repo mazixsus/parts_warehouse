83ac3d6cec5fa9f87b919bffc38e0177699ce392
mateusz <mateusz3249@gmail.com>
2024-02-14 21:36:10 +0100

    categories search done

--- src/categories/views.py
+++ src/categories/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 from rest_framework_mongoengine import viewsets
+from rest_framework import filters
+
 
 from .serializers import CategoriesSerializer
 from .models import Category
 
+
+class IsOwnerFilterBackend(filters.BaseFilterBackend):
+    """
+    Filter that only allows users to see their own objects.
+    """
+    def filter_queryset(self, request, queryset, view):
+        print(request)
+        name = request.query_params.get('name')
+        parent = request.query_params.get('parent')
+
+        if name:
+            queryset = queryset.filter(name=name)
+
+        if parent:
+            queryset = queryset.filter(parent=parent)
+
+        return queryset
+
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategoriesSerializer
     lookup_field = 'name'
+    filter_backends = [IsOwnerFilterBackend]
     
-    # queryset = Category.objects.all()
     def get_queryset(self):
         return Category.objects.all()
